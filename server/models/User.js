5cb98d4b58e751a89bcf9600646fd643b187307d
Moksha K <110051904+Moksha-Kachhia@users.noreply.github.com>
2024-08-20 15:52:26 -0400

    Created user database and password handling (encryption and password checking) methods.

---
+++ server/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose; 
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    firstName:String,
+    lastName:String,
+    username:{type:String,required:true},
+    password:{type:String,required:true}
+})
+
+
+userSchema.pre("save",async function(next){ //hashing new password 
+    const user = this; 
+    if(!user.isModified) return next(); //data is fresh
+    let salt = await bcrypt.genSalt(10); 
+    let hash = await bcrypt.hash(user.password,salt); 
+    user.password = hash; 
+    next(); 
+})
+
+
+userSchema.compPassward = async function(password){ //confirming password 
+    return bcrypt.compare(password, this.password);
+}
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- server/server.js
+++ server/server.js
@@ -2,18 +2,18 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config;
+require('dotenv').config();
 
 const PORT = process.env.PORT || 4000
 
 app.use(cors());
 app.use(express.json()); 
 
-// mongoose.connect(process.env.DB_URL).then((result)=>{
-//     console.log("DB connected sucessfully!");
-// }).catch(err=>{
-//     console.log(err);
-// })
+mongoose.connect(process.env.DB_URL).then((result)=>{
+    console.log("DB connected sucessfully!");
+}).catch(err=>{
+    console.log(err);
+})
 
 app.listen(PORT,()=>{
     console.log(`Server started at port ${PORT}`);
