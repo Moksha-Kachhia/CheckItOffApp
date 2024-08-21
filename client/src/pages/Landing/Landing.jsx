f20807062832d3ca48dbba01fd1786dbc605dcc5
Moksha K <110051904+Moksha-Kachhia@users.noreply.github.com>
2024-08-20 22:08:14 -0400

    Make landing page look nicer and edit README.

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import NavBar from '../../components/NavBar';
+import {Link} from 'react-router-dom'
+import landing from '../../assets/Landing.jpg';
+import styles from './Landing.module.css';
 
 function Landing() {
   return (
     <div>
-      <NavBar />
-      <section>
-        <div>
-          <h1>Landing</h1>
-        </div>
-        <div>
+      <NavBar active={"home"}/>
+        <div className={styles.landing__wrapper}>
+            <div className={styles.landing__text}>
+                <h1>Improve Your Time Management With <span className="primaryText">CheckItOff!</span></h1>
+            <div className='btnWrapper'>
+            <Link to="/register" className="primaryBtn">Register</Link>
+                <Link to="/login" className='secondaryBtn'>Login</Link>
+            </div>
+            </div>
 
-        </div>
-      </section>
+            <div className={styles.landing__img}>
+                <img src={landing} alt="landing" />
+            </div>
+      </div>
     </div>
   )
 }
